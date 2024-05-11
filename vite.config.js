b9e577410fd71b13612f37e9f3bff16a0402ab87
Anuj Dagade <dagadeanuj@gmail.com>
2024-05-11 16:25:33 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,50 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  // vite.config.js
+
+  server: {
+    host: "0.0.0.0", // Set host to '0.0.0.0' to make it available for the entire network
+    port: 3000, // Optionally, set the port number
+  },
+
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      manifest: {
+        name: "Vite PWA Project",
+        short_name: "Vite PWA Project",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "pwa-64x64.png",
+            sizes: "64x64",
+            type: "image/png",
+          },
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any",
+          },
+          {
+            src: "maskable-icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
+});
